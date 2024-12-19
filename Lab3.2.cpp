67ca50d18885a0572d00bd22f6218cc3a83fb345
Poomwillbeprogrammer <poomwingame@gmail.com>
2024-12-19 11:17:55 +0700

    commit

--- Lab3.2.cpp
+++ Lab3.2.cpp
@@ -3,29 +3,29 @@
 
 struct student
 {
-    char name[20];
-    int age;
-    char sex;
-    float gpa;
-};
+    char name[20] ;
+    int age ;
+    char sex ;
+    float gpa ;
+} ;// end struct
 
 void upgrade(struct student *child);
 
 int main()
 {
-    struct student aboy;
-    aboy.sex = 'M';
-    aboy.gpa = 3.00;
-    upgrade(&aboy);
-    printf("%.2f", aboy.gpa);
-    return 0;
+    struct student aboy ;
+    aboy.sex = 'M' ;
+    aboy.gpa = 3.00 ;
+    upgrade( &aboy ) ;
+    printf( "%.2f", aboy.gpa ) ;
+    return 0 ;
 } // end function
 
-void upgrade(struct student *child) {
-    if (child->sex == 'M') {
-        child->gpa *= 1.10;
+void upgrade( struct student *child ) {
+    if (child ->sex == 'M') {
+        child ->gpa *= 1.10;
     }
-    else if (child->sex == 'F') {
-        child->gpa *= 1.20;
+    else if ( child->sex == 'F' ) {
+        child ->gpa *= 1.20 ;
     }
-}
+}//end function
--- Lab3.4.cpp
+++ Lab3.4.cpp
@@ -2,53 +2,53 @@
 
 struct student
 {
-    char name[20];
-    int age;
-};
+    char name[20] ;
+    int age ;
+};//end struct
 
-void GetStudent(struct student child[][10], int *room);
+void GetStudent(struct student child[][10], int *room) ;
 
 int main()
 {
-    struct student children[20][10];
+    struct student children[20][10] ;
     int group;
-    GetStudent(children, &group);
+    GetStudent( children, &group ) ;
 
     printf("\n-----------------------------------------------------\n");
-    for (int i = 0; i < group; i++) 
+    for ( int i = 0; i < group; i++ ) 
     {
-        printf("Group %d:\n", i + 1);
-        for (int j = 0; j < 3; j++) 
+        printf("Group %d:\n", i + 1 ) ;
+        for ( int j = 0; j < 3; j++ ) 
         {
-            printf("Student %d: Name = %s, Age = %d\n", j + 1, children[i][j].name, children[i][j].age);
-        }
-    }
+            printf("Student %d: Name = %s, Age = %d\n", j + 1, children[i][j].name, children[i][j].age) ;
+        }//end for
+    }//end for
     printf("\n-----------------------------------------------------\n");
-    return 0;
+    return 0 ;
 } // end function
 void GetStudent(struct student child[][10], int *room)
 {
     int recroom = 0;
 
-    printf("Enter the number of groups: ");
-    scanf("%d", room);
+    printf("Enter the number of groups: ") ;
+    scanf("%d", room) ;
 
     for (int i = 0; i < *room; i++)
     {
-        printf("Room %d\n", recroom + 1);
+        printf("Room %d\n", recroom + 1) ;
         for (int j = 0; j < 10; j++)
         {
-            printf("student %d: ", j + 1);
-            scanf("%s", child[i][j].name);
-            printf("Enter age: \n");
-            scanf("%d", &child[i][j].age);
-        }
+            printf( "student %d: " , j + 1) ;
+            scanf( "%s" , child[i][j].name) ;
+            printf( "Enter age: \n" );
+            scanf( "%d" , &child[i][j].age) ;
+        }//end for
         
         recroom++;
-    }
+    }//end for
 
         for (recroom = 0; recroom < *room; recroom++)
         {
             printf("Room %d: 10 students entered.\n", recroom + 1);
-        }
-}
+        }//end for
+}//end functiom
